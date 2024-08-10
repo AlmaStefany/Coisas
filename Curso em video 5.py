1483571abd068fff36cf1963880bd91082a0e9f0
vitoria <vitoria.higino6@gmail.com>
2024-08-10 17:30:16 -0300

    Operadores aritmétricos e Ordem de precedência

---
+++ Curso em video 5.py
@@ -0,0 +1,18 @@
+'''     Operadores aritmétricos
+
++ adição
+- subtração
+* multiplicação
+/ divisão
+** potência
+// divisão inteira
+% resto da divisão
+
+        Ordem de precedência
+
+1 ()
+2 **
+3 * / // %
+4 + -
+
+'''
