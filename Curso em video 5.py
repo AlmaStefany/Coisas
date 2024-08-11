337755b848e33c0e511b5562a71af92f6b20d95b
vitoria <vitoria.higino6@gmail.com>
2024-08-11 20:35:54 -0300

    Operadores aritmétricos e Ordem de precedência pt2

--- Curso em video 5.py
+++ Curso em video 5.py
@@ -16,3 +16,14 @@
 4 + -
 
 '''
+
+
+teste = input("Digite seu nome: ")
+print("Prazer em te conhecer {:>20}!".format(teste)) #essa função faz alinhamento a 20 caracteres á direita >
+# o sinal ^ alinha ao centro de 20 espaços
+
+n1 = int(input("um valor: "))
+n2 = int(input("outro valor"))
+
+
+#parei nos 25:59 de video.
