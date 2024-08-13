175fee4aec75edf7a84df95b3649a17d2b8f29d8
vitoria <vitoria.higino6@gmail.com>
2024-08-13 19:17:30 -0300

    Operadores aritmétricos e Ordem de precedência pt4

--- Curso em video 5.py
+++ Curso em video 5.py
@@ -28,4 +28,28 @@ print("a soma é {}: ".format(n1+n2), end="")#esse end impede a quebra de linha,
 print("teste pra impedir a quebra de linha")
 print("teste pra quebrar a linha ")#basta colocar \n no inicio e no final doq vc quer q esteja em outra linha exemplo: Olá \n td bem? \n
 
-'''Exercicios 005'''
+'''                 Exercicio 005                 '''
+
+n = int(input("Digite um numero: "))
+a = n - 1
+s = n + 1
+print("o valor é {}, seu antecessor é {} e o sucessor é {}".format(n, a, s))
+
+# outra forma de fazer
+n = int(input("digite um numero: "))
+print("o valor é {}, seu antecessor é {} e o sucessor é {}".format(n, (n-1), (n+1)))
+
+
+'''             Exercicio 006                      '''
+
+n = int(input("Digite um numero: "))
+d = n * 2 #dobro
+t = n * 3 #triplo
+r = n ** (1/2) #raiz quadrada
+print("O dobro de {} vale {}".format(n, d))
+print("O triplo de {} vale {} \n A raiz quadrada de {} é igual a {:.2}".format(n, t, n, r)) #esse :.2 limita a raiz quadrada mostrar apenas 2 casas decimais flutuantes
+
+# outra forma de fazer
+n = int(input("Digite um valor"))
+print("O dobro de {} vale {} ".format(n, (n*2)))
+print("o triplo de {} vale {} \n a raiz quadada de {} é igual a {} ".format(n, (n*3), n, (n**(1/2))))
