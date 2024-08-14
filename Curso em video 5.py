d39ecd373607c9592a04e66d29cee3ac891b2441
vitoria <vitoria.higino6@gmail.com>
2024-08-14 19:04:54 -0300

    Operadores aritmétricos e Ordem de precedência pt5

--- Curso em video 5.py
+++ Curso em video 5.py
@@ -53,3 +53,11 @@ print("O triplo de {} vale {} \n A raiz quadrada de {} é igual a {:.2}".format(
 n = int(input("Digite um valor"))
 print("O dobro de {} vale {} ".format(n, (n*2)))
 print("o triplo de {} vale {} \n a raiz quadada de {} é igual a {} ".format(n, (n*3), n, (n**(1/2))))
+
+
+'''         Exercicio 007                          '''
+
+n1 = float(input("primeira nota do aluno: "))
+n2 = float(input("segunda nota do aluno: "))
+média = (n1 + n2) /2
+print("a média entre {} e {} é igual a {} ".format(n1, n2, média))
